542f43613963e20e3e4803d85463fc5c6d83334b
Ashkan <AshkanWatson@gmail.com>
2025-11-21 19:17:53 +0330

    Adding Supabase Constants

---
+++ lib/utils/constants.dart
@@ -0,0 +1,7 @@
+// Configuration constants
+const String kSupabaseUrl = 'YOUR_SUPABASE_URL';
+const String kSupabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+
+// Database constants
+const String kDatabaseName = 'vaultly_v2.db';
+const String kTableName = 'credentials';
